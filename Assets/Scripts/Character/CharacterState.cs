76cf1ce1f32b49bf91d5bd8ebe31247cd54a5459
kwakminkyu <kmk3296@gmail.com>
2026-07-29 23:59:44 +0900

    [add] 스테이트 시스템 기반 추가

---
+++ Assets/Scripts/Character/CharacterState.cs
@@ -0,0 +1,22 @@
+public abstract class CharacterState
+{
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Update()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void OnAttackContact()
+    {
+    }
+
+    public virtual void OnAnimationFinished()
+    {
+    }
+}
